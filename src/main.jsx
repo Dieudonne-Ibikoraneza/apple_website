38087333d57186a4bd6448fe775b31c0501b1989
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-10-06 22:30:15 +0200

    Successfully set up Sentry for the project

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,10 @@ Sentry.init({
   dsn: "https://799fbd4f2e954d4bd34aa7cde7b95915@o4508077627539456.ingest.us.sentry.io/4508077629571072",
   integrations: [
     Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
     Sentry.replayIntegration(),
   ],
   tracesSampleRate: 1.0,
